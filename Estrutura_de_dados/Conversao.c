11c765eacc12990383aacd0fc02be98cfabc5472
Luiz Herran <102183730+LuizHerran@users.noreply.github.com>
2025-04-24 10:06:12 -0300

    letra_em_ascii

---
+++ Estrutura_de_dados/Conversao.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char letra = 'A';
+    int ascii = (int)letra;
+
+    printf("O caractere '%c' tem o valor ASCII %d\n", letra, ascii);
+    return 0;
+}
--- Estrutura_de_dados/selectionsort.c
+++ Estrutura_de_dados/selectionsort.c
@@ -3,8 +3,14 @@
 int main(){
 
     int vet[]={10,5,1,3,7,2,4,6,8,9};
-    int min, aux;
+    int min, max, aux;
     int tam = sizeof(vet) / sizeof(vet[0]);
+
+    printf("Valores originais: ");
+    for(int i = 0; i < tam; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\r=================================================\n");
 
     for(int i = 0; i < tam-1; i++){
         min=i;
@@ -18,9 +24,30 @@ int main(){
             vet[i]=vet[min];
             vet[min]=aux;
             }
-
     }
+printf("Valores crescentes: ");
 for(int i = 0; i < tam; i++){
     printf("%d ", vet[i]);
 }
+printf("\r=================================================");
+printf("\rValores decrescentes: ");
+
+for(int i = 0; i < tam-1; i++){
+    max=i;
+    for(int j = i+1; j < tam; j++){
+        if(vet[j] > vet[max]){
+            max=j;
+        }
+    }
+    if(max != i){
+        aux=vet[i];
+        vet[i]=vet[max];
+        vet[max]=aux;
+        }
+
+}
+for(int i = 0; i < tam; i++){
+    printf("%d ", vet[i]);
+}
+printf("\r=================================================");
 }
